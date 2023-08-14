e10ad0d3c00e326426d10f7bfe022da6d1837cdf
Rohit Rawat <107769619+professorSergio12@users.noreply.github.com>
2023-08-15 02:18:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,46 @@ const downloadBtn = document.getElementById('downloadBtn');
 const qrContainer = document.querySelector('.qr-body');
 
 let size = sizes.value;
-generateBtn.addEventListener('click', (e)=>{
-     e.preventDefault();
-     generateQRCode();
+generateBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    isEmptyInput()
 })
 
-function generateQRCode(){
-    qrContainer.innerHTML = "";
-    new QRCode(qrContainer,{
-        text:qrText.value,
+sizes.addEventListener('change', (e) => {
+    size = e.target.value;
+    isEmptyInput()
+    // generateQRCode();
+})
+
+function isEmptyInput() {
+    if (qrText.value.length > 0) {
+
+        generateQRCode();
+    }
+    else {
+        alert("Enter the text or URL to generate your QR code")
+    }
+}
+
+downloadBtn.addEventListener('click', () => {
+    let img = document.querySelector('.qr-body img');
+    if (img !== null) {
+        let imgAtr = img.getAttribute('src')
+        downloadBtn.setAttribute("href", imgAtr)
+    }
+    else {
+        downloadBtn.setAttribute("href", `${document.querySelector('canvas').toDataURL()}`)
+    }
+})
+
+function generateQRCode() {
+    qrContainer.innerHTML = ""; 
+    new QRCode(qrContainer, {
+        text: qrText.value,
         height: size,
         width: size,
-        colorLight:"#fff",
+        colorLight: "#fff",
         colorDark: "#000"
-
     });
 }
+
